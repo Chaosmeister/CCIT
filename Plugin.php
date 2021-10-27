b36d3bff4c96e15f80ef9d09f3960c08f4b851fc
Tomas Dittmann <tomas.dittmann@kisters.de>
2021-10-27 13:15:09 +0200

    V 1.0

---
+++ Plugin.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Kanboard\Plugin\CCIT;
+
+use Kanboard\Core\Plugin\Base;
+
+class Plugin extends Base
+{
+    public function initialize()
+    {
+        $this->hook->on('template:layout:js', array('template' => 'plugins/CCIT/Assets/js/CCIT.js'));
+
+        $this->route->addRoute('PITM/Checkbox', 'CheckboxController', 'toggle', 'CCIT');
+    }
+
+    public function getPluginName()
+    {
+        return "ClickableCheckboxInTextAreas";
+    }
+
+    public function getPluginAuthor()
+    {
+        return 'Tomas Dittmann';
+    }
+
+    public function getPluginVersion()
+    {
+        return '1.0.0';
+    }
+
+    public function getPluginDescription()
+    {
+        return 'Clickable boxes in Textareas';
+    }
+}
